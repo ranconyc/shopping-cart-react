d4a0308257f15e024bfc7a2514e3900d320cbc4d
ran cohen <ranco.nyc@gmail.com>
2020-07-07 03:46:31 +0300

    create product component

--- shopping-cart-react/src/App.js
+++ shopping-cart-react/src/App.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import data from './data.json';
+import Products from './components/Products';
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href='/'> React Shopping Cart </a>
-      </header>
-      <main>
-         Product List
-      </main>
-      <footer>
-      All right is reserved Rancode2020.
-      </footer>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products: data.products,
+      size: '',
+      sort: '',
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href='/'> React Shopping Cart </a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products}></Products>
+            </div>
+            <div className="sidebar">
+              cart items
+            </div>
+          </div>
+        </main>
+        <footer>
+        All right is reserved Rancode2020.
+        </footer>
+      </div>
+    );
+  }
 }
-
 export default App;
